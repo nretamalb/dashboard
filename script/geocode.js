02e2e8005f3bd7ba1c544de5222501c811c64714
nretamalb <nretamalbarberis@gmail.com>
2022-10-25 15:08:31 -0300

    Modular

--- script/geocode.js
+++ script/geocode.js
@@ -1,41 +1,68 @@
-(function () {
-  //----Elementos input de la pagina pagina----
-  let cityInput = document.getElementById("ciudad");
-  let countryInput = document.getElementById("pais");
-  //-----------------------------
-  // ---Variables que van al link de la API---
-  let cityName = "";
-  let countryName = "";
-  const apiKey = "cb6ea339cd642c30d5f67cb1f287b8ee";
-  let url = "";
-  //-------------------------
-  //----Funcion que obtiene datos de la ciudad solicitada----
-  const get = () => {
-    //-----Tomar valores de ciudad desde el input------
-    cityName = cityInput.value;
-    countryName = countryInput.value;
-    //-----Aplicar valolres de input a url de la API-------
-    url =
-      "http://api.openweathermap.org/geo/1.0/direct?q=" +
-      cityName +
-      "," +
-      countryName +
-      "&limit=5&appid=" +
-      apiKey;
+//----Elementos input de la pagina pagina----
+let cityInput = document.getElementById("ciudad");
+let countryInput = document.getElementById("pais");
+//-----------------------------
+// ---Variables que van al link de la API---
+let cityName = "";
+let countryName = "";
+let lon = "";
+let lat = "";
 
-    //------Promesa que entrega latitud y longitud de la ciudad
-    return Promise.resolve(
-      fetch(url)
-        .then((response) => {
-          return response.json();
-        })
-        .catch((err) => {
-          console.error(err);
-        })
-    );
-  };
-});
+const apiKey = "cb6ea339cd642c30d5f67cb1f287b8ee";
+let url = "";
+//-------------------------
+
+//----Objeto con informacion de la ciudad----
+//let cityData = new Object();
+let cityData = [];
+//----Funcion que obtiene datos de la ciudad solicitada----
+const get = () => {
+  //-----Tomar valores de ciudad desde el input------
+  cityName = cityInput.value;
+  countryName = countryInput.value;
+  //-----Aplicar valolres de input a url de la API-------
+  url =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityName +
+    "," +
+    countryName +
+    "&limit=5&appid=" +
+    apiKey;
+
+  //------Promesa que entrega latitud y longitud de la ciudad
+  return Promise.resolve(
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  );
+};
+const getCity = () => {
+  return get().then((data) => {
+    cityName = data[0].name;
+    countryName = data[0].country;
+    lon = data[0].lon;
+    lat = data[0].lat;
+    cityData.push(cityName);
+    cityData.push(countryName);
+    cityData.push(lat);
+    cityData.push(lon);
+
+    // cityData = {
+    //   cityName: cityName,
+    //   countryName: countryName,
+    //   lat: lat,
+    //   lon: lon,
+    // };
+    console.log("cityData Executed");
+    return cityData;
+  });
+  //Creo un objeto con los valores obtenidos de respuesta y exporto
+  //ese objeto para utilizarlo en index.js
+};
 
 //----Exportamos variables relevantes y funcion----
-
-export { cityName, cityInput, get };
+export { getCity, cityData };
---
+++ script/graphTemp.js
@@ -0,0 +1,113 @@
+import { cityData } from "./geocode.js";
+//Elemento canvas de tarjeta que muestra temperaturas maximas
+const graph1 = document.querySelector("#myChart1");
+
+//----Obtenemos informacion de la temperatura utilizando la api correspondiente----
+const getTempData = async (infoCity) => {
+  console.log(infoCity);
+  let lat = infoCity[2];
+  let lon = infoCity[3];
+  console.log(lon);
+  return await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=afa815d7eedd497bfe6a8d94b49ed7d2`
+  ).then((data) => data.json());
+};
+
+/*---- Tomamos informacion entregada por la api y exportamos la informacion
+correspondiente a las fechas y sus temperaturas para ese dia----*/
+const dataRefactoring = (infoCity) => {
+  getTempData(infoCity).then((response) => {
+    let fecha = [
+      ...new Set(response.map((element) => element.dt_txt.split(" ")[0])),
+    ];
+
+    let today = new Date();
+    let ddToday = String(today.getDate()).padStart(2, "0");
+
+    let tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    let ddTomorrow = String(tomorrow.getDate()).padStart(2, "0");
+
+    let filtro;
+    let arrayDivisionPorFechas = [];
+
+    // Esto es porque la api no está sincronizada con el horario local
+    if (response[0].dt_txt.split(" ")[0].includes(`-${ddToday}`)) {
+      for (let i = 0; i < fecha.length; i++) {
+        today = Number(ddToday) + i;
+
+        filtro = response.filter((element) =>
+          element.dt_txt.includes(`-${today}`)
+        );
+
+        arrayDivisionPorFechas.push(filtro);
+      }
+    } else {
+      for (let i = 0; i < fecha.length; i++) {
+        tomorrow = Number(ddTomorrow) + i;
+
+        filtro = response.filter((element) =>
+          element.dt_txt.includes(`-${tomorrow}`)
+        );
+
+        arrayDivisionPorFechas.push(filtro);
+      }
+    }
+
+    let valorFechaPrimeraVez = arrayDivisionPorFechas.map(
+      (element) => element[0]
+    );
+
+    let temperatura = valorFechaPrimeraVez.map(
+      (element) => element.main.temp_max
+    );
+
+    let temperaturaCelcius = valorFechaPrimeraVez.map(
+      (element) => element.main.temp_max - 273.15
+    );
+    console.log(fecha);
+    console.log(temperaturaCelcius);
+    return { fecha, temperaturaCelcius };
+  });
+};
+
+//---- Generamos grafico con la informacion ya filtrada
+const maxTempsGraph = (infoCity) => {
+  console.log("maxTempsGraph Executed");
+  dataRefactoring(infoCity);
+  const graphTemp = new Chart(graph1, {
+    type: "bar",
+    data: {
+      labels: fecha,
+      datasets: [
+        {
+          label: `Temperatura ${infoCity[0]} en °C`,
+          data: temperaturaCelcius,
+          backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+          borderColor: ["rgb(54, 162, 235)"],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      responsive: true,
+      plugins: {
+        legend: {
+          labels: {
+            font: {
+              size: 14,
+              weight: 600,
+            },
+          },
+        },
+      },
+    },
+  });
+};
+
+export { maxTempsGraph };
